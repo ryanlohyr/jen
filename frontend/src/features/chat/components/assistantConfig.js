23e6ad718ceb128519de7db9c8cab9ed1bd7aafe
Ryan Loh <ryanloh29@gmail.com>
2024-10-20 05:47:08 -0700

    Modify config

--- frontend/src/features/chat/components/assistantConfig.js
+++ frontend/src/features/chat/components/assistantConfig.js
@@ -22,7 +22,54 @@ export const assistantOptions = (userProfile) => {
           `,
         },
       ],
-      toolIds: ["6ad122d0-c202-402b-9f07-720625c47705"],
+      // toolIds: ["6ad122d0-c202-402b-9f07-720625c47705"],
+      tools: [
+        {
+          type: "function",
+          messages: [
+            {
+              type: "request-start",
+              content:
+                "Checking relevant doctor information for the web now. Please wait...",
+            },
+            {
+              type: "request-complete",
+              content:
+                "I was able to get the relevant doctor information. These are the options available:",
+            },
+            {
+              type: "request-failed",
+              content:
+                "I'm sorry, I was unable to get the relevant doctor information. Please try again later.",
+            },
+            {
+              type: "request-response-delayed",
+              content:
+                "I'm sorry, getting the information is taking longer than expected. Please wait a little longer.",
+              timingMilliseconds: 2000,
+            },
+          ],
+          function: {
+            name: "getDoctorInfo",
+            parameters: {
+              type: "object",
+              properties: {
+                user_query: {
+                  type: "string",
+                },
+                user_context: {
+                  type: "object",
+                },
+              },
+            },
+            description: "Gets relevant doctor information for your condition.",
+          },
+          async: false,
+          server: {
+            url: "https://jen-calhacks-backend.onrender.com/search/search",
+          },
+        },
+      ],
     },
   };
 };
