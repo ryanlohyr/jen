e1c77d7daffdaa930345dc3c0f9c76b91dfa849b
Ryan Loh <ryanloh29@gmail.com>
2024-10-20 06:06:27 -0700

    Fix

--- backend/backend/src/search_service/search_routes.py
+++ backend/backend/src/search_service/search_routes.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter
 from .search_agent import SearchAgent
 from pydantic import BaseModel
+
 router = APIRouter(prefix="/search")
 import logging
 import json
 from fastapi import Request
+
 logger = logging.getLogger(__name__)
 
 search_router = APIRouter(prefix="/search")
@@ -13,9 +15,11 @@ search_router = APIRouter(prefix="/search")
 #     user_query: str
 #     user_context: str
 
+
 class SearchRequest(BaseModel):
     response: str
     user_context: str
+
 
 @search_router.post("/search")
 async def search(request: Request):
@@ -23,23 +27,29 @@ async def search(request: Request):
     try:
         print(f"Request body: {await request.body()}")
         user_request = json.loads(await request.body())
-        
+
         # user_data = user_request["message"]["toolWithToolCallList"][0]["function"]["parameters"]["properties"]
         print("ze request is ")
         print(user_request["message"]["toolCallList"])
-        user_query = user_request["message"]["toolCallList"][0]["function"]["arguments"]["user_query"]
-        user_context = "Health insurance provider is Blue Shield, Has a Medicare Supplement Plan"
+        user_query = user_request["message"]["toolCallList"][0]["function"][
+            "arguments"
+        ]["user_query"]
+        user_context = (
+            "Health insurance provider is Blue Shield, Has a Medicare Supplement Plan"
+        )
         
+        tool_id = user_request["message"]["toolCallList"][0]["id"]
+
     except json.JSONDecodeError as e:
         logger.error(f"Error decoding JSON: {e}")
         print(f"Error decoding JSON: {e}")
         return {"error": f"Invalid JSON: {e}"}
-    
+
     search_agent = SearchAgent()
     results = search_agent.search(user_query, user_context)
-    
-    print(f"Results: {results}")
-    
-    results = {"results": json.dumps(results)}
-    
-    return results
+
+
+
+    response = {"results": [{"toolCallId": tool_id, "result": json.dumps(results)}]}
+
+    return response
